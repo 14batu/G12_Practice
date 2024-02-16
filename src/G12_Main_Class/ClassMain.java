3fd45b5cdea45ebcdef1a4bbd554efbb05b5345a
User <uljana555@mail.ru>
2024-02-16 13:03:16 -0500

    changes added

--- src/G12_Main_Class/ClassMain.java
+++ src/G12_Main_Class/ClassMain.java
@@ -3,5 +3,6 @@ package G12_Main_Class;
 public class ClassMain {
     public static void main(String[] args) {
         //Main Class
+        System.out.println("Uliana worked on this file");
     }
 }
---
+++ src/G12_Main_Class/UlianasClass.java
@@ -0,0 +1,7 @@
+package G12_Main_Class;
+
+public class UlianasClass {
+    public static void main(String[] args) {
+        System.out.println("I love Java");
+    }
+}
