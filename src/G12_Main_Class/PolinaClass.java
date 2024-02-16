43fdadbb2692ad62464c5b950a670af0f0bb2bbd
irinayarema <MykolaYarema@yahoo.com>
2024-02-16 15:37:58 -0500

    Updated Polina's class.

--- src/G12_Main_Class/PolinaClass.java
+++ src/G12_Main_Class/PolinaClass.java
@@ -1,7 +1,11 @@
 package G12_Main_Class;
 
+import java.sql.SQLOutput;
+
 public class PolinaClass {
     public static void main(String[] args) {
+
         System.out.println("Hello, team!");
+        System.out.println("Hi Polina") ; // Irina
     }
 }
