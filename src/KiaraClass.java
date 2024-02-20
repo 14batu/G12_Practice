a88bdd8086583a99a1f6a6089d6f6f260df12867
kiara <kikilorena49@yahoo.com>
2024-02-20 09:13:38 -0500

    Created KiaraCLass.01

---
+++ src/KiaraClass.java
@@ -0,0 +1,8 @@
+public class KiaraClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, this is Kiara's first class");
+        System.out.println("Made a branch from local develop");
+    }
+}
